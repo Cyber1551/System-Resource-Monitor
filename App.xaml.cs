0e21a261ce32e1eff2d5e248a5b453f235937af4
Cyber1551 <develop@brandonlacy.com>
2026-05-19 13:28:23 -0500

    create WPF app and add .idea to gitignore

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace System_Resource_Monitor;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
